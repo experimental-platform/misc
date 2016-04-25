632a27fffda992004afddbe7b45da4de6f0bea5f
Florian Aßmann <florian.assmann@email.de>
2016-04-25 14:15:00 +0200

    Install those reporters.

--- test/buildimg_test.rb
+++ test/buildimg_test.rb
@@ -5,7 +5,7 @@ require 'tempfile'
 
 require 'minitest'
 require 'minitest/autorun'
-require 'minitest/reporters' rescue nil
+require 'minitest/reporters'
 
 ROOT = File.expand_path '../..', __FILE__
 
