ad407a078805fbb952811ee160edfe14ac2ffcc8
Kamil Domański <kamil@domanski.co>
2016-08-10 10:32:26 +0200

    install-glide.sh: allow to pick version, but default to 0.6.1

--- install-glide.sh
+++ install-glide.sh
@@ -5,7 +5,13 @@ set -eux
 TMPFILE=$(mktemp)
 TMPDIR=$(mktemp -d)
 
-curl -L https://github.com/Masterminds/glide/releases/download/0.6.1/glide-linux-amd64.zip > "$TMPFILE"
+URL="https://github.com/Masterminds/glide/releases/download/0.6.1/glide-linux-amd64.zip"
+
+if [ $# -eq 1 ]; then
+  URL="https://github.com/Masterminds/glide/releases/download/$1/glide-$1-linux-amd64.zip"
+fi
+
+curl -L "$URL" > "$TMPFILE"
 unzip "$TMPFILE" -d "$TMPDIR"
 mkdir -p "$HOME/bin"
 echo 'export PATH="$PATH:$HOME/bin"' >>  "$HOME/.profile"
