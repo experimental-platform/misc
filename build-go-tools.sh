66500befc7053d7a9b5db5305b8c689ea079e14a
Hinnerk Haardt <haardt@information-control.de>
2016-08-22 13:58:51 +0200

    Make the build path configurable.

--- build-go-tools.sh
+++ build-go-tools.sh
@@ -4,12 +4,13 @@ set -eu
 set -o pipefail
 
 export GO15VENDOREXPERIMENT=1
+BASEPATH=${BASEPATH:-go}
 
 curl -L https://raw.githubusercontent.com/experimental-platform/misc/master/install-glide.sh | bash -s v0.11.1
 
 mkdir -p binaries
 
-for tooldir in go/*; do
+for tooldir in ${BASEPATH}/*; do
   tool_name="$(basename $tooldir)"
   echo "Building $tool_name"
 
