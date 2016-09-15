412e9c0b0ae2432e5e179ab16c63f0b7d9c4984c
Hinnerk Haardt <haardt@information-control.de>
2016-09-15 12:01:07 +0200

    Go to go 1.7

--- build-go-tools.sh
+++ build-go-tools.sh
@@ -4,7 +4,6 @@ set -eu
 set -o pipefail
 set -x
 
-export GO15VENDOREXPERIMENT=1
 BASEPATH=${BASEPATH:-go}
 
 curl -L https://raw.githubusercontent.com/experimental-platform/misc/master/install-glide.sh | bash -s v0.11.1
@@ -16,11 +15,11 @@ for tooldir in ${BASEPATH}/*; do
   echo "Building $tool_name"
 
   echo " * Gliding up"
-  docker run -v "$HOME/bin/glide:/usr/bin/glide:ro" -v "$(readlink -f $tooldir):/go/src/$tool_name" -w "/go/src/$tool_name" -e GO15VENDOREXPERIMENT=1 golang:1.5 glide up
+  docker run -v "$HOME/bin/glide:/usr/bin/glide:ro" -v "$(readlink -f $tooldir):/go/src/$tool_name" -w "/go/src/$tool_name" golang:1.7 glide up
   echo " * Building"
-  docker run -v "$HOME/bin/glide:/usr/bin/glide:ro" -v "$(readlink -f $tooldir):/go/src/$tool_name" -w "/go/src/$tool_name" -e GO15VENDOREXPERIMENT=1 golang:1.5 go build
+  docker run -v "$HOME/bin/glide:/usr/bin/glide:ro" -v "$(readlink -f $tooldir):/go/src/$tool_name" -w "/go/src/$tool_name" golang:1.7 go build
   echo " * Testing"
-  docker run -v "$HOME/bin/glide:/usr/bin/glide:ro" -v "$(readlink -f $tooldir):/go/src/$tool_name" -w "/go/src/$tool_name" -e GO15VENDOREXPERIMENT=1 golang:1.5 go test -v
+  docker run -v "$HOME/bin/glide:/usr/bin/glide:ro" -v "$(readlink -f $tooldir):/go/src/$tool_name" -w "/go/src/$tool_name" golang:1.7 go test -v
 
   mv "$tooldir/$tool_name" "binaries/"
 done
