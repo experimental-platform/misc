b8925e4cb570fd6ab6134f9dce01a96acf3cd0b1
Kamil Domański <kamil@domanski.co>
2016-09-22 16:30:23 +0200

    bump to Go 1.7

--- go-build.sh
+++ go-build.sh
@@ -7,8 +7,8 @@ PROJECT_NAME="github.com/$TRAVIS_REPO_SLUG"
 export GO15VENDOREXPERIMENT=1
 curl -L https://raw.githubusercontent.com/experimental-platform/misc/master/install-glide.sh | sh
 cp $HOME/bin/glide .
-docker run -v "${SRC_PATH}:/go/src/$PROJECT_NAME" -w "/go/src/$PROJECT_NAME" -e GO15VENDOREXPERIMENT=1 golang:1.6 /bin/bash -c "./glide install && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -v -ldflags='-s -w'"
-docker run -v "${SRC_PATH}:/go/src/$PROJECT_NAME" -w "/go/src/$PROJECT_NAME" -e GO15VENDOREXPERIMENT=1 golang:1.6 go test -v
+docker run -v "${SRC_PATH}:/go/src/$PROJECT_NAME" -w "/go/src/$PROJECT_NAME" -e GO15VENDOREXPERIMENT=1 golang:1.7 /bin/bash -c "./glide install && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -v -ldflags='-s -w'"
+docker run -v "${SRC_PATH}:/go/src/$PROJECT_NAME" -w "/go/src/$PROJECT_NAME" -e GO15VENDOREXPERIMENT=1 golang:1.7 go test -v
 
 wget 'https://github.com/Yelp/dumb-init/releases/download/v1.0.1/dumb-init_1.0.1_amd64' -O dumb-init && chmod +x dumb-init
 
